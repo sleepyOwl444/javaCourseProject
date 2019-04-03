29b9f97b53a589d2efcb232f0df2aab8f9c82ff6
Pichkur Ekaterina <pichkurkatya@gmail.com>
2019-04-04 00:39:22 +0300

    first commit

---
+++ src/main/java/com/pichkur/javaCourse/JavaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.pichkur.javaCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichkur/javaCourse/JavaCourseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pichkur.javaCourse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaCourseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
