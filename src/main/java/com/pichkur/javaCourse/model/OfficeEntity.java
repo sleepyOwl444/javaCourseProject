113ebb3676a64b204614e93174859798cd8c41b5
Pichkur Ekaterina <pichkurkatya@gmail.com>
2019-04-08 01:19:28 +0300

    Исправила import (по правилам wildcard использовать нельзя)

--- src/main/java/com/pichkur/javaCourse/model/OfficeEntity.java
+++ src/main/java/com/pichkur/javaCourse/model/OfficeEntity.java
@@ -6,7 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.FetchType;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
